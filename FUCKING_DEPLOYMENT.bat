d51a0310a12eae32eff3d76eb9662f3654b1b775
Matthias <rocketpencil.studios@gmail.com>
2021-11-29 01:35:12 +0100

    Test deployment

--- FUCKING_DEPLOYMENT.bat
+++
@@ -1,6 +0,0 @@
-set /P version="Version: "
-call mvn install:install-file -DgroupId=com.github.polyrocketmatt -DartifactId=Peak -Dversion=%version% -Dfile="..\..\Peak\target\peak.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
-git add *
-git commit -m "released dev build %version%"
-git pull
-git push
