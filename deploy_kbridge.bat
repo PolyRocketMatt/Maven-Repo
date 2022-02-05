f277893b32e863caed0ec336e2ee7d904f6375c8
PolyRocketMatt <rocketpencil.studios@gmail.com>
2022-02-05 13:08:56 +0100

    Fix some things

--- deploy_kbridge.bat
+++
@@ -1,6 +0,0 @@
-set /P version="Version: "
-call mvn install:install-file -DgroupId=com.github.polyrocketmatt -DartifactId=KotlinBridge -Dversion=%version% -Dfile="..\..\KotlinBridge\target\KotlinBridge.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
-git add *
-git commit -m "Released KotlinBridge %version% (dev)"
-git pull
-git push
